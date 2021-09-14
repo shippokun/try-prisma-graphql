e4eae76175a83a32856dbea95f399fea3d2d6de8
shippokun <okashi0tabetai@gmail.com>
2021-09-15 00:29:58 +0900

    chore: play index

--- index.ts
+++ index.ts
@@ -3,8 +3,42 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 async function main() {
-  const allUser = await prisma.user.findMany();
-  console.log(allUser);
+  await prisma.user.create({
+    data: {
+      name: "John",
+      email: "john@prisma.io",
+      posts: {
+        create: { title: "Hello World" },
+      },
+      profile: {
+        create: { bio: "JohnTitor" },
+      },
+    },
+  });
+
+  const allUsers = await prisma.user.findMany({
+    include: {
+      posts: true,
+      profile: true,
+    },
+  });
+
+  console.dir(allUsers, { depth: null });
+}
+
+async function update() {
+  const getPost = await prisma.post.findFirst({
+    where: { id: 1 },
+  });
+
+  console.log(getPost);
+
+  const post = await prisma.post.update({
+    where: { id: 1 },
+    data: { publish: true },
+  });
+
+  console.log(post);
 }
 
 main()
